b7df39cb875469294656def6879fa8115706eb6d
Khomas5 <106009864+Khomas5@users.noreply.github.com>
2022-09-19 12:41:40 +0400

    updt

--- js/main.js
+++ js/main.js
@@ -51,3 +51,48 @@ const headerEl = document.querySelector(".header");
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
+
+
+// STICKY HEADER
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function () {
+  myFunction();
+};
+
+// Get the header
+let header = document.querySelector(".header-box");
+let headerBox = document.querySelector(".header");
+
+// Get the offset position of the navbar
+let sticky = headerBox.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+      if(!headerBox.classList.contains("sticky") && !document.body.classList.contains("nav-open")){
+        headerBox.classList.add("sticky");
+}
+  } else {
+      if(headerBox.classList.contains("sticky") && !document.body.classList.contains("nav-open")){
+        headerBox.classList.remove("sticky");
+   }
+  }
+}
+
+// window.onscroll = function() {myFunction()};
+
+// // Get the header
+// var header = document.getElementById(".header-box");
+
+// // Get the offset position of the navbar
+// var sticky = header.offsetTop;
+
+// // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// function myFunction() {
+//   if (window.pageYOffset > sticky) {
+//     header.classList.add("sticky");
+//   } else {
+//     header.classList.remove("sticky");
+//   }
+// }
